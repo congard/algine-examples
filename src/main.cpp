408721adaf9d3e53f67d9e2ba647102f95f8042a
Daniel Bernar <dbcongard@gmail.com>
2020-07-08 18:58:43 +0300

    Update inclusions

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <tulz/Path>
+#include <tulz/Path.h>
 
 #include <algine/core/Engine.h>
 #include <algine/core/shader/ShaderProgram.h>
