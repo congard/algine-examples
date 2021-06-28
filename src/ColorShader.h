f8dfde23b5aa50d664a3b867eced3462ef145e75
Daniel Bernar <dbcongard@gmail.com>
2021-06-28 14:17:53 +0300

    Update ColorShader constants

--- src/ColorShader.h
+++ src/ColorShader.h
@@ -3,39 +3,27 @@
 
 #define constant(name, val) constexpr char name[] = val;
 
-namespace ColorShader {
-    namespace Settings {
-        constant(BoneSystem, "ALGINE_BONE_SYSTEM")
-        constant(Lighting, "ALGINE_LIGHTING_MODE_ENABLED")
-        constant(SSR, "ALGINE_SSR_MODE_ENABLED")
-        constant(TextureMapping, "ALGINE_TEXTURE_MAPPING_MODE_ENABLED")
-        constant(TextureMappingSwitcher, "ALGINE_TEXTURE_MAPPING_MODE_DUAL")
-    }
+namespace ColorShader::Vars {
+constant(ModelMatrix, "modelMatrix")
+constant(ViewMatrix, "viewMatrix")
+constant(MVPMatrix, "MVPMatrix")
+constant(MVMatrix, "MVMatrix")
+constant(InPos, "inPos")
+constant(InNormal, "inNormal")
+constant(InTexCoord, "inTexCoord")
+constant(InTangent, "inTangent")
+constant(InBitangent, "inBitangent")
 
-    namespace Vars {
-        constant(ModelMatrix, "modelMatrix")
-        constant(ViewMatrix, "viewMatrix")
-        constant(MVPMatrix, "MVPMatrix")
-        constant(MVMatrix, "MVMatrix")
-        constant(InPos, "inPos")
-        constant(InNormal, "inNormal")
-        constant(InTexCoord, "inTexCoord")
-        constant(InTangent, "inTangent")
-        constant(InBitangent, "inBitangent")
-
-        constant(AmbientTex, "ambient")
-        constant(DiffuseTex, "diffuse")
-        constant(SpecularTex, "specular")
-        constant(NormalTex, "normal")
-        constant(ReflectionStrengthTex, "reflectionStrength")
-        constant(JitterTex, "jitter")
-        constant(AmbientStrength, "ambientStrength")
-        constant(DiffuseStrength, "diffuseStrength")
-        constant(SpecularStrength, "specularStrength")
-        constant(Shininess, "shininess")
-
-        constant(CameraPos, "cameraPos")
-    }
+constant(AmbientTex, "ambient")
+constant(DiffuseTex, "diffuse")
+constant(SpecularTex, "specular")
+constant(NormalTex, "normal")
+constant(ReflectionStrengthTex, "reflectionStrength")
+constant(JitterTex, "jitter")
+constant(AmbientStrength, "ambientStrength")
+constant(DiffuseStrength, "diffuseStrength")
+constant(SpecularStrength, "specularStrength")
+constant(Shininess, "shininess")
 }
 
 #undef constant
